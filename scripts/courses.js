5c09909eba5d1b9aecda413abef68f68c32c61e1
rmora06 <robertjouelmora@gmail.com>
2026-01-10 12:45:57 -0400

    week01_project

---
+++ scripts/courses.js
@@ -0,0 +1,44 @@
+const container = document.getElementById("courses");
+const totalCreditsSpan = document.getElementById("totalCredits");
+
+function displayCourses(list) {
+  container.innerHTML = "";
+
+  const total = list.reduce((sum, course) => sum + course.credits, 0);
+  totalCreditsSpan.textContent = total;
+
+  list.forEach(course => {
+    const card = document.createElement("div");
+    card.classList.add("course-card");
+
+    if (course.completed) {
+      card.classList.add("completed");
+    }
+
+    card.innerHTML = `
+      <h3>${course.subject} ${course.number}</h3>
+      <p>${course.title}</p>
+      <p>${course.credits} credits</p>
+    `;
+
+    container.appendChild(card);
+  });
+}
+
+// Show all when loads
+displayCourses(courses);
+
+// Buttons
+document.getElementById("all").addEventListener("click", () => {
+  displayCourses(courses);
+});
+
+document.getElementById("wdd").addEventListener("click", () => {
+  const filtered = courses.filter(c => c.subject === "WDD");
+  displayCourses(filtered);
+});
+
+document.getElementById("cse").addEventListener("click", () => {
+  const filtered = courses.filter(c => c.subject === "CSE");
+  displayCourses(filtered);
+});
---
+++ scripts/navigation.js
@@ -0,0 +1,6 @@
+const menuBtn = document.getElementById("menuBtn");
+const nav = document.getElementById("mainNav");
+
+menuBtn.addEventListener("click", () => {
+  nav.classList.toggle("open");
+});
