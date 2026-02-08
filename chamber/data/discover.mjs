677e303a9cc2a0392d10b06db8d8d72c0882c88a
rmora06 <robertjouelmora@gmail.com>
2026-02-07 20:50:48 -0400

    chamber final

---
+++ chamber/data/discover.mjs
@@ -0,0 +1,50 @@
+export const places = [
+  {
+    name: "Catedral de la Vega",
+    image: "images/catedral.webp",
+    address: "Av. Antonio Guzmán Fernández, La Vega 41000",
+    description: "Religous Place. Unique Catedral in Town"
+  },
+  {
+    name: "Santo Cerro",
+    image: "images/santo_cerro.webp",
+    address: "Calle las Mercedes santo cerro, La Vega",
+    description: "Historic Place near one the first cities in America"
+  },
+  {
+    name: "Piramides de Valle Nuevo",
+    image: "images/piramides.webp",
+    address: "Valle Nuevo, Constaza",
+    description: "Ancient Piramids with great Value for the Community."
+  },
+  {
+    name: "Divino Nino",
+    image: "images/divino_nino.webp",
+    address: "Mountain, Constanza, La Vega",
+    description: "Jesus statute with many visitors around the year."
+  },
+  {
+    name: "La Vega Vieja",
+    image: "images/ruinas.webp",
+    address: "La Vega Vieja, Concepcion de La Vega",
+    description: "First community of America after Colon."
+  },
+  {
+    name: "Museo del Carnaval Vegano",
+    image: "images/museo.webp",
+    address: "Palacio Don Zoilo, Jose Horacio Rodriguez Esq, Duvergé 41000",
+    description: "Memmories of the Most Important Carnaval on the Caribbean."
+  },
+  {
+    name: "Parque de las Flores",
+    image: "images/flores.webp",
+    address: "Center of the City, La Vega",
+    description: "This park in right in the middle of the City. Surprisingly, it does have any flowers."
+  },
+  {
+    name: "Crurch of Jesus Christ of Latter Day Saints",
+    image: "images/sud.webp",
+    address: "Jose Horacio Rodriguez, La Vega 41000",
+    description: "First SUD church of the City."
+  }
+];
